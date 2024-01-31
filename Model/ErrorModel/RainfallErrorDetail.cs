b3a58e4c0c1b6516126a385f6f3c48e4899487ba
Drazie <front.kd19@gmail.com>
2024-01-31 20:44:19 +0200

    base models added

---
+++ Model/ErrorModel/RainfallErrorDetail.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RainfallREST.Model.ErrorModel
+{
+	public class RainfallErrorDetail
+	{
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+    }
+}
+
